3fa9c962494ca5f3c9015ee91788ae1d953fc2b6
GarismoHussien <hand2land@gmail.com>
2024-11-03 12:25:39 +0200

    T

---
+++ Traing FILE/testing2.py
@@ -0,0 +1,10 @@
+# Code to calculate the average of three numbers
+num1 = 10
+num2 = 20
+num3 = 30
+
+# Calculate the average
+average = num1 + num2 + num3 / 3  
+
+# Display the result
+print("The average of the three numbers is:", average)
