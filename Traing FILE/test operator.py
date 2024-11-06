6ef934d0567972e17b1906d7ac8cf3ede1d417f7
GarismoHussien <hand2land@gmail.com>
2024-11-06 18:15:41 +0200

    T

---
+++ Traing FILE/test operator.py
@@ -0,0 +1,23 @@
+# Example 1: Division Operator Mistake
+result = 5 // 2
+print("Result of 5 / 2 is:", result)
+
+# Example 2: Concatenation Operator Mistake
+age = 25
+message = "Your age is " + age
+print(message)
+
+# Example 3: Comparison Operator Mistake
+num = 10
+if num = 10:
+    print("Number is 10")
+
+# Example 4: Logical Operator Mistake
+x = 5
+y = 10
+if x > 0 and y < 15:
+    print("Condition met: x is positive or y is less than 15")
+
+# Example 5: Exponentiation Operator Mistake
+value = 4 ** 3
+print("Value of 4 * 3 is:", value)
