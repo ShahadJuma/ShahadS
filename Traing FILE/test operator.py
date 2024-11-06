427a9e02d3bfa7c27022ec6f7c30303ef2266e9d
ShahadJuma <shahadalnuaimi89@hotmail.com>
2024-11-06 20:59:30 +0400

    updated

--- Traing FILE/test operator.py
+++ Traing FILE/test operator.py
@@ -27,38 +27,46 @@ if x > 0 or y < 15:
     print("Condition met: x is positive or y is less than 15")
 
 
-
-
-
-
 # Example 5: Exponentiation Operator Mistake
 value = 4 * 3
 print("Value of 4 * 3 is:", value)
 
 
+
+
 # Example 6: Modulus Operator Mistake
+# Incorrect: Using / instead of % to check if a number is even.
 number = 10
-if number / 2 == 0:
+if number % 2 == 0:
     print("Number is even")
 
 # Example 7: Logical NOT Operator Mistake
-is_logged_in = True
-if not is_logged_in == True:
+# Incorrect: Using "not" inappropriately with a comparison.
+
+is_logged_in = False
+if not is_logged_in:
     print("User is not logged in")
+else:
+    print("User is logged in")
+
 
 # Example 8: Bitwise Operator Mistake
+# Incorrect: Using & (bitwise AND) instead of and (logical AND).
 a = 5
 b = 10
-if a > 0 & b < 15:
+if a > 0 and b < 15:
     print("Condition met")
 
+    
 # Example 9: Increment Operator Mistake
+# Incorrect: Trying to use ++ (not valid in Python).
 counter = 0
-++counter
+counter += 1
 print("Counter is:", counter)
 
 # Example 10: Assignment Operator Mistake
+# Incorrect: Trying to use the compound assignment operator with a string and integer.
 text = "Score: "
 score = 10
-text += score
+text += str(score)
 print(text)
