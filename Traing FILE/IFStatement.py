de81d69e9474c79ff04bd7e1201b0f813f3e38eb
ShahadJuma <shahadalnuaimi89@hotmail.com>
2024-11-20 20:40:15 +0400

    updated

--- Traing FILE/IFStatement.py
+++ Traing FILE/IFStatement.py
@@ -30,3 +30,13 @@ if age >= 18:
     print("You are an adult.")
 else:
     print("You are a minor.")
+
+
+    # Using break and continue in a loop
+for number in range(1, 11):
+    if number == 5:
+        print("Found 5, breaking the loop.")
+        break  # Exit the loop when the number is 5
+    if number % 2 == 0:
+        continue  # Skip even numbers
+    print(f"Odd number: {number}")
